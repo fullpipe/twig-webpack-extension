3d5dd474afaea6abc8542c6ca2020c7c83871515
Eugene Bravov <eugene.bravov@gmail.com>
2020-04-27 09:19:17 +0300

    Fix bug with publicPath

--- example/frontend/src/second.js
+++ example/frontend/src/second.js
@@ -1,11 +1,8 @@
-import "./index.css";
-
-import _ from "lodash";
 import $ from "jquery";
 
 $(() => {
   const $element = $("<div></div>");
 
-  $element.text(_.join(["Hello", "from", "index.js"], " "));
+  $element.text(_.join(["Hello", "from", "second.js"], " "));
   $element.hide().appendTo($(".content")).fadeIn("slow");
 });
--- example/frontend/webpack.config.js
+++ example/frontend/webpack.config.js
@@ -6,23 +6,24 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: {
     vendor: ["jquery", "lodash"],
-    main: "./src/index.js"
+    main: "./src/index.js",
+    second: "./src/second.js",
   },
   output: {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "../public/build"),
-    publicPath: "/build/"
+    publicPath: "/build/",
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor",
-      minChunks: Infinity
+      minChunks: Infinity,
     }),
     new ManifestPlugin(),
     new ExtractTextPlugin({
       filename: "css/[name].css",
-      publicPath: "/build/"
-    })
+      publicPath: "/build/",
+    }),
   ],
   module: {
     rules: [
@@ -30,9 +31,9 @@ module.exports = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: "css-loader"
-        })
-      }
-    ]
-  }
+          use: "css-loader",
+        }),
+      },
+    ],
+  },
 };
